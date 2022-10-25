a1d94379a8570fb17337ea5ac5d8569839ea4113
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-10-25 14:26:26 +0100

    typerefs

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
 const typeDefs = `#graphql
-
   type Charecter {
-    id: Number,
+    id: number,
     name: String,
     marriage: Boolean,
     topGifts: [String],
