3486efcd27c9b545edbcd3cf98dfeeeac7cf08c2
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-10-25 14:17:55 +0100

    removed comment

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
 const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
-  # This "Book" type defines the queryable fields for every book in our data source.
   type Charecter {
     id: number,
     name: string,
